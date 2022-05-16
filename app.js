5fa7a881fc1e93ff219d83f1baa7e76feeba1769
Jumanazar <jsr1611@outlook.com>
2022-05-16 21:07:11 +0900

    added app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+const api_keys = window.API_KEYS;
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ", " + city;
+
+    
+    console.log(api_keys)
+    
+    $greeting.text('So, you want to live at ' + address + '?');
+    var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?key='+ api_keys.GOOGLE_API_KEY+'&size=600x300&location=' + address + '';
+    $('.bgimg').attr("src", streetViewUrl);
+
+    var headline = "New York Times Articles About " + city;
+    $nytHeaderElem.text(headline);
+
+    var NYT_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocations:"+ city+ "&api-key=" + api_keys.NYT_API_KEY;
+    
+    $.getJSON(NYT_url, function(data){
+        // console.log(data.response.docs);
+        var items = [];
+        $.each(data.response.docs, function(key, val){
+            items.push("<li id='" + key + "'>" + '<a target="_blank" href="' + val.web_url +'">'+ val.headline.main + "</a><p>" + val.lead_paragraph + "</p></li>");
+        });
+        
+        $nytElem.append(items);
+        console.log("items: " +  items.length);
+    });
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
+
+// loadData();
